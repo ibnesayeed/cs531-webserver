cc5c63b49ee0e48a38cb522501c052fa8616e9f6
Sawood Alam <ibnesayeed@gmail.com>
2018-09-05 13:03:01 -0400

    Make the server host and port configurable

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM python:alpine
 COPY server.py /
 RUN  chmod a+x /server.py
 
-CMD ["/server.py"]
+CMD ["/server.py", "0.0.0.0", "80"]
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
 import socket
+import sys
 
-HOST = "0.0.0.0"
-PORT = 80
+HOST = "localhost"
+PORT = 8080
+
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+if len(sys.argv) > 2:
+    PORT = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -23,7 +29,10 @@ Hello, World!
 
 while True:
     conn, addr = s.accept()
-    req = conn.recv(1024)
-    print(req)
+    while True:
+        req = conn.recv(1024)
+        print(req)
+        if req.strip() == b"":
+            break
     conn.sendall(res.encode("utf-8"))
     conn.close()
