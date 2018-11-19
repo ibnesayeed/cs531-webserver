041e05e31de88ff3d557c42536f64bbb854734a0
Sawood Alam <ibnesayeed@gmail.com>
2018-11-19 11:39:42 -0500

    Restructure bootstrapping

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
-FROM python:alpine
+FROM    python:alpine
 
-ENV  PYTHONUNBUFFERED=1
+ENV     PYTHONUNBUFFERED=1
 
-COPY server.py bootstrap.sh /
-RUN  chmod +x /server.py /bootstrap.sh
-RUN  /bootstrap.sh
+WORKDIR /app
+COPY    . ./
+RUN     chmod +x *.py *.sh
+RUN     ./bootstrap.sh
 
-CMD ["/server.py", "0.0.0.0", "80"]
+CMD     ["./server.py", "0.0.0.0", "80"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 SAMPLEDIR="./sample"
 PUBLICDIR="./public"
+
+# Ensure that PUBLICDIR exists before extracting sample *.tar.gz files in it
+mkdir -p $PUBLICDIR
 
 echo "Bootsrapping web root directory with sample data"
 
 for f in $SAMPLEDIR/*.tar.gz
 do
   echo "Extracting sample file $f"
-  tar xvzf -C $PUBLICDIR $f
+  tar -C $PUBLICDIR -xvzf $f
 done
 
 echo "Sample files extracted in the web root"
